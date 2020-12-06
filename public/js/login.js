4a752fb67b7e6beea311e343a00a37fbc6b02115
Kaan Yilmaz <kaan.yilmaz@ormuco.com>
2020-12-06 00:05:22 -0800

    login page css

--- public/js/login.js
+++ public/js/login.js
@@ -5,9 +5,10 @@ const setAction = (action) =>{
         form.submit()
     }
     else{
+        document.querySelector("#error-notification").style.display = "block"
         document.querySelector("#error-notification").textContent= "Both Username and password fields must be completed!"
         document.querySelector("#error-notification").setAttribute("style","display: block") 
     }
 }
-document.querySelector(".login").addEventListener('click',(ev)=>setAction("/login"))
-document.querySelector(".signup").addEventListener('click',(ev)=>setAction("/signup"))
+document.querySelector("#login").addEventListener('click',(ev)=>setAction("/login"))
+document.querySelector("#signup").addEventListener('click',(ev)=>setAction("/signup"))
