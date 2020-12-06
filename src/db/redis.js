90afb1c3ce619ef716695557e54c3177c2c2336e
Kaan Yilmaz <kaan.yilmaz@ormuco.com>
2020-12-06 00:54:28 -0800

    redis server connection updated

--- src/db/redis.js
+++ src/db/redis.js
@@ -1,9 +1,10 @@
 const { model } = require("../models/user");
 const redis = require("redis");
 
+const redisUrl= process.env.REDIS_URL.split(":")
 const client = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
+  host: redisUrl[0],
+  port: redisUrl[1],
 });
 
 client.on("error", (error) => {
